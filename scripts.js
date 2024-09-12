bf1495e0c3515071d6348e7b26c5085ba8c01c96
nzoops <garry.enzo.m@gmail.com>
2024-09-12 14:03:27 +0100

    updates 1209

--- scripts.js
+++ scripts.js
@@ -19,3 +19,25 @@ function scrollRight() {
 
 document.querySelector('.scroll-left').addEventListener('click', scrollLeft);
 document.querySelector('.scroll-right').addEventListener('click', scrollRight);
+
+//script.js
+
+// The text you want to display in typewriter effect
+const text = "I love building tech products that make technical topics look simple.";
+
+// Select the paragraph element where text will appear
+const typewriterElement = document.querySelector('#typewriter p');
+
+let index = 0;
+
+// Function to gradually display the text character by character
+function type() {
+  if (index < text.length) {
+    typewriterElement.textContent += text.charAt(index); // Add one character at a time
+    index++;
+    setTimeout(type, 100); // Adjust typing speed by changing the timeout value
+  }
+}
+
+// Call the type function when the window loads
+window.onload = type;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./src/**/*.{html,js}", // Adjust to match your file structure
-    ],
-    theme: {
-      extend: {
-        keyframes: {
-          typewriter: {
-            '0%': { width: '0%' },
-            '100%': { width: '100%' },
-          },
-          blink: {
-            '0%': { borderColor: 'transparent' },
-            '50%': { borderColor: 'black' },
-          },
-        },
-        animation: {
-          typewriter: 'typewriter 4s steps(40, end) 1s 1 normal both',
-          blink: 'blink .75s step-end infinite',
-        },
-        fontFamily: {
-          typewriter: ['"Courier New"', 'Courier', 'monospace'],
-        },
-      },
-    },
-    plugins: [],
-  }
-  
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//       "./src/**/*.{html,js}", // Adjust to match your file structure
+//     ],
+//     theme: {
+//       extend: {
+//         keyframes: {
+//           typewriter: {
+//             '0%': { width: '0ch' },
+//             '100%': { width: '55ch' },
+//           },
+//           blink: {
+//             '0%': { borderColor: 'transparent' },
+//             '50%': { borderColor: 'black' },
+//           },
+//         },
+//         animation: {
+//           typewriter: 'typewriter 4s steps(55, end) 1s 1 normal both',
+//           blink: 'blink .75s step-end infinite',
+//         },
+//         fontFamily: {
+//           typewriter: ['"Courier New"', 'Courier', 'monospace'],
+//         },
+//       },
+//     },
+//     plugins: [],
+//   }
